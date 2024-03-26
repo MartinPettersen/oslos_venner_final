bf0dcac4d56a1dac7143c2f275c4beefed84bc76
MartinPettersen <searchingtheskyfor@gmail.com>
2024-03-26 11:20:17 +0100

    add denied page

---
+++ app/Denied/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center text-4xl sm:text-9xl text-danger-red font-bold'>Denied</div>
+  )
+}
+
+export default page
