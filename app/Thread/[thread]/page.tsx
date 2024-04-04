c2f2dfbef9f6a92a3d81fe8d705e231d1ff146b1
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-04 14:25:23 +0200

    add css thead page

--- app/Thread/[thread]/page.tsx
+++ app/Thread/[thread]/page.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-
+import React from "react";
 
 type Props = {
-    params: { threadId: string };
-  };
+  params: { threadId: string };
+};
 
 const page = ({ params }: Props) => {
   const threadId = params.threadId;
 
   return (
-    <div className='w-full h-full '>page</div>
-  )
-}
+    <div className="w-full h-screen  flex items-center justify-center flex-col gap-2">
+      <div className="bg-green rounded-md p-2 text-soft-pink">thread subject</div>
+      <div className="flex flex-col gap-2">
+        <div className="bg-blue rounded-md p-2 text-soft-pink">replies</div>
+        <div className="bg-blue rounded-md p-2 text-soft-pink">replies</div>
+        <div className="bg-blue rounded-md p-2 text-soft-pink">replies</div>
+      </div>
+    </div>
+  );
+};
 
-
-export default page
+export default page;
