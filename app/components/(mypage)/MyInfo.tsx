b836db21a2fb11f2e2c0366c7f7ed8c189fd27a7
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-05 08:13:30 +0200

    bug fix for vercel deployment

--- app/components/(mypage)/MyInfo.tsx
+++ app/components/(mypage)/MyInfo.tsx
@@ -9,10 +9,10 @@ const MyInfo = ({user}: Props) => {
   return (
     <div className='w-[173px] sm:w-[573px] h-[70px] p-2 sm:h-[243px] text-[10px] sm:text-[40px] dark:border dark:border-pink overflow-auto flex flex-col justify-center bg-blue dark:bg-transparent text-soft-pink dark:text-grey items-start rounded-xl sm:rounded-3xl dark:rounded-none'>
         <div>
-            {`Bruker Navn: ${user.name}`} 
+            {`Bruker Navn: ${user?.name}`} 
         </div>
         <div>
-        {`E-post: ${user.email}`}
+        {`E-post: ${user?.email}`}
         </div>
         <div>
             Status: Ikke Verifisert
