fe6c64fb5b1260103081d72c343def735fdc2f42
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-08 10:09:30 +0200

    add menu for replies

--- app/components/(reply)/ReplyDisplayCard.tsx
+++ app/components/(reply)/ReplyDisplayCard.tsx
@@ -1,6 +1,7 @@
 import { Reply } from '@/types/Reply'
 import React from 'react'
 import TimeStamp from '../(util)/TimeStamp'
+import ReplyDotMenu from './ReplyDotMenu'
 
 type Props = {
     reply?: Reply
@@ -11,7 +12,7 @@ const ReplyDisplayCard = ({reply}: Props) => {
     <div className="bg-blue w-[80%] text-xs rounded-md p-2 dark:bg-gradient-to-r from-orange to-pink text-soft-pink dark:text-dark-grey dark:rounded-none">
         <div className='flex flex-row justify-between'>
             <div>{reply?.userName}</div>
-            <div>{`\u00B7\u00B7\u00B7`}</div>
+            <ReplyDotMenu reply={reply} />
         </div>
         <div>
         {reply?.reply}
