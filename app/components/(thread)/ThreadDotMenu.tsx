1436e40d3d0d3c358bc06058ab0bcc7f423bdaf7
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-08 10:14:28 +0200

    fix bug with location for dot menu

--- app/components/(thread)/ThreadDotMenu.tsx
+++ app/components/(thread)/ThreadDotMenu.tsx
@@ -20,7 +20,7 @@ const ThreadDotMenu = () => {
             className="flex z-[10] absolute top-0 left-0 w-screen h-screen "
           ></div>
 
-          <div className="flex flex-col items-center justify-center p-2 rounded-xl dark:rounded-none gap-1 z-[100] fixed right-[5%] h-30 w-30 bg-green dark:bg-gradient-to-b from-dark-grey to-black">
+          <div className="flex flex-col items-center justify-center p-2 rounded-xl dark:rounded-none gap-1 z-[100] fixed sm:fixed right-[5%] sm:right-auto h-30 w-30 bg-green dark:bg-gradient-to-b from-dark-grey to-black">
 
             <div className="bg-light-brown rounded-full dark:rounded-none px-4 cursor-pointer dark:bg-gradient-to-r from-orange to-pink text-soft-pink dark:text-dark-grey w-full flex items-center justify-center">Rapporter</div>
             <div className="bg-light-brown rounded-full dark:rounded-none px-4 cursor-pointer dark:bg-gradient-to-r from-orange to-pink text-soft-pink dark:text-dark-grey w-full flex items-center justify-center">Del</div>
