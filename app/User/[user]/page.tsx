e3b305d014c6c8a51af8cb08bf349167ed71b1eb
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-08 11:13:34 +0200

    add css for user page

--- app/User/[user]/page.tsx
+++ app/User/[user]/page.tsx
@@ -9,19 +9,25 @@ type Props = {
 const page = ({ params }: Props) => {
   const userName = params.user;
 
-
   return (
-    <div className="flex justify-center flex-col gap-4 sm:gap-10 w-screen items-center p-4">
-      <div className="bg-slate-500  p-4 w-full sm:w-[60%] flex">
-      
-        <div className="w-full flex flex-col gap-2 sm:gap-4 sm:p-4 ">
-          <h1 className="text-orange-300 font-bold text-4xl flex items-center justify-center ">
+    <div className="flex  flex-col  w-screen h-screen items-center p-4">
+      <div className="h-1/5"></div>
+
+      <div className="bg-green dark:bg-gradient-to-r from-orange to-pink w-[90%] p-4 rounded-xl dark:rounded-none flex flex-col gap-4 items-center justify-center">
+        <div className="flex w-full items-center justify-center">
+          <div className="w-1/6 "></div>
+          <div className="text-xl w-4/6 flex justify-center items-center text-soft-pink dark:text-dark-grey">
             {userName.replace("%20", " ")}
-          </h1>
+          </div>
+          <div className="w-1/6">...</div>
+        </div>
+
+        <div className="flex flex-col sm:flex-col gap-2"> 
+            <div className="bg-brown dark:bg-gradient-to-b from-dark-grey to-black text-soft-pink dark:text-pink p-2 rounded-full dark:rounded-none flex items-center justify-center">Kommentarer</div>
+            <div className="bg-brown dark:bg-gradient-to-b from-dark-grey to-black text-soft-pink dark:text-orange p-2 rounded-full dark:rounded-none flex items-center justify-center">Tråder</div>
 
         </div>
       </div>
-      
     </div>
   );
 };
