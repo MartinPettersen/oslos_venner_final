033cc52ba6f7438487a20e582587c00dec9d8db2
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-10 09:54:54 +0200

    fix bug where content on threadpage was to low

--- app/Thread/[thread]/page.tsx
+++ app/Thread/[thread]/page.tsx
@@ -37,7 +37,6 @@ const page = ({ params }: Props) => {
 
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col gap-4">
-      <div className="h-[15rem]"></div>
       <ThreadDisplayCard thread={thread} />
       
       <ThreadRepliesContainer thread={thread} />
