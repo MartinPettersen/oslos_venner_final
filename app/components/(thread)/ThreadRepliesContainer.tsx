af84258c3bf40a9aa1e36bb42a9d92e1fa096205
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-11 09:25:07 +0200

    fix thread replies bug

--- app/components/(thread)/ThreadRepliesContainer.tsx
+++ app/components/(thread)/ThreadRepliesContainer.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const ThreadRepliesContainer = ({thread}: Props) => {
   return (
-    <div className="flex flex-col gap-2 overflow-scroll no-scrollbar overflow-y-auto items-center">
+    <div className="flex flex-col  w-full  sm:w-[40%] h-[40%]  sm:h-[50%] gap-2 overflow-scroll no-scrollbar overflow-y-auto items-start justify-start">
       {thread?.replies.map((reply, index) => (
 
           <ReplyDisplay replyId={reply} key={index}/>
