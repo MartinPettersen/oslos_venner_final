9193f7e66bd68a958749e1b8121c507c48298f97
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-16 13:57:49 +0200

    allow users to delete their account

--- app/CreateUser/page.tsx
+++ app/CreateUser/page.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import CreateUserForm from '../components/(createuser)/CreateUserForm'
+import React from "react";
+import CreateUserForm from "../components/(createuser)/CreateUserForm";
 
 const page = () => {
   return (
-    <div className='w-screen h-screen flex flex-col gap-4 items-center  justify-center'>
-        
-        <div className='flex flex-col items-center justify-center   w-full  sm:w-[40%] h-full sm:h-[100%]'>
-
-        <div className='text-3xl sm:text-6xl p-2 text-light-brown dark:text-grey'>Ny Bruker</div>
+    <div className="w-screen h-screen flex flex-col gap-4 items-center  justify-center">
+      <div className="flex flex-col items-center justify-center   w-full  sm:w-[40%] h-full sm:h-[100%]">
+        <div className="text-3xl sm:text-6xl p-2 text-light-brown dark:text-grey">
+          Ny Bruker
+        </div>
         <CreateUserForm />
-        
-        </div>
-        </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- app/MyPage/page.tsx
+++ app/MyPage/page.tsx
@@ -6,6 +6,7 @@ import MyInfo from "../components/(mypage)/MyInfo";
 
 import { useSession } from 'next-auth/react'
 import { redirect } from "next/navigation";
+import DeleteButton from "../components/(util)/DeleteButton";
 
 const page = () => {
 
@@ -23,6 +24,7 @@ const page = () => {
       <div className="w-[90%] h-[75%] p-4  items-center justify-center flex flex-col gap-[30px] sm:gap-[60px]">
         <MyInfo user={session?.user!} />
         <DeleteAccount />
+        <DeleteButton subjectType={"user"} subjectId={session?.user!.name!} label={"Slett Bruker"} />
       </div>
     </div>
   );
