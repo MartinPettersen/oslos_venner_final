b14c3a50a595e3f4c8e9874cb046228f11e0fe76
MartinPettersen <searchingtheskyfor@gmail.com>
2024-04-17 08:31:08 +0200

    improve visibility on hamburger menu

--- app/components/(navigation)/NavMenu.tsx
+++ app/components/(navigation)/NavMenu.tsx
@@ -4,7 +4,7 @@ import NavLinks from "./NavLinks";
 
 const NavMenu = () => {
   return (
-    <div className="flex flex-col sm:flex-row items-center justify-center gap-4 text-white dark:text-grey text-[32px] font-semibold">
+    <div className="bg-light-brown  rounded-md p-2 dark:bg-gradient-to-t from-orange to-pink text-soft-pink dark:text-dark-grey dark:rounded-none flex flex-col sm:flex-row items-center justify-center gap-4  text-[32px] font-semibold">
       <NavLinks />
     </div>
   );
